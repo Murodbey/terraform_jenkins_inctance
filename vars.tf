0b338e8adce6ea8d291439bd02f6135ae233763c
murodbey <abdullayevmurod@gmail.com>
2018-11-03 18:34:24 +0000

    instance

---
+++ vars.tf
@@ -0,0 +1,15 @@
+variable "region" {}
+variable "instancetype" {}
+variable "count" {}
+
+
+
+variable "amis" {
+  type = "map"
+
+  default = {
+    eu-west-1 = "ami-6e28b517"
+    us-east-1 = "ami-4bf3d731"
+    us-west-2 = "ami-e1496384"
+  }
+}
